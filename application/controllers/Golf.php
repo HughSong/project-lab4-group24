67b19b80d943d1dfb805faf0f9f762c6b1863758
juntrc <junnan.tang.2014@gmail.com>
2017-09-28 17:11:52 -0700

    closes #8
    Signed-off-by: juntrc <junnan.tang.2014@gmail.com>

---
+++ application/controllers/Golf.php
@@ -0,0 +1,18 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+class Golf extends Application
+{
+
+	function __construct()
+	{
+		parent::__construct();
+	}
+
+
+	public function index()
+	{
+		$this->show(6);
+	}
+
+}
