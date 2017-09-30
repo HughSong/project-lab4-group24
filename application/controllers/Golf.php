ed350ae9412cd324cb57739d60ca1d6895cdc909
juntl <jtang145@my.bcit.ca>
2017-09-29 20:00:24 -0700

    close issue#9 hotel add comment
    Signed-off-by: juntl <jtang145@my.bcit.ca>

--- application/controllers/Golf.php
+++ application/controllers/Golf.php
@@ -1,7 +1,20 @@
 <?php
 defined('BASEPATH') OR exit('No direct script access allowed');
 
-//Golf's controller 
+/*
+ * Golf's and Hotel's controller
+ *
+ * For Golf:
+ * This will be a routing managed with a rule
+ * Make the target link /i/need/a/hobby
+ * Add a routing rule to config/routes.php, which remaps any request with the first segment "i" to the Golf controller
+ * The body of that method should invoke the inherited show method, with the key "6"
+ *
+ * For Hotel:
+ * This will be a routing managed with a rule
+ * Make the target link /bananas/rule
+ * Add a routing rule to config/routes.php, which remaps any request with the word "banana" in it
+ */
 class Golf extends Application
 {
 
